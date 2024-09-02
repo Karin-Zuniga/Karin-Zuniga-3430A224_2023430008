6debb8a859bf822508ab275d823abae8d1f24df9
karin-zuniga <karin.zuniga.munoz@gmail.com>
2024-09-02 17:57:20 -0400

    funciones dlist agregadas, listo

--- paciente_dlist.cpp
+++ paciente_dlist.cpp
@@ -35,7 +35,12 @@ void eliminarPaciente(Paciente*& head, const string& nombre){
        {
         head = actual->next;
         }
+        else
+        {
+            anterior->next = actual->next;
+        }
         delete actual;
+        
         
     }
 }
@@ -47,30 +52,72 @@ void imprimirPaciente(const Paciente* head){
         cout << "Edad: " << actual->edad << "\n";
         cout << "Peso: " << actual->peso << "\n";
         cout << "Altura: " << actual->altura << "\n";
-        cout << "---------------- \n";        
+        cout << "---------------- \n";
+        actual = actual->next;        
 
     }
 }
 
 int main () {
+    int menu = 1;
+    int opcion;
+    
+    Paciente* head = nullptr;
+
+    while ( menu != 0)
+    {
+        cout << "Bienvenido, que desea hacer? \n";
+        cout << "0. Salir\n";
+        cout << "1. Agregar Paciente \n";
+        cout << "2. Mostrar informacion \n";
+        cout << "3. Eliminar Paciente \n";
+        cin >> opcion;
+
+        if (opcion == 0)
+        {
+            break;
+        }
+        //opcion 1. agrega datos nuevo paciente
+        else if (opcion == 1)
+        {
+            string nombre;
+            int edad;
+            double peso;
+            double altura;
+            cout << "Nombre \n";
+            cin >> nombre;
+            cout << "edad \n";
+            cin >> edad;
+            cout << "peso \n";
+            cin >> peso;
+            cout << "altura \n";
+            cin >> altura;
+
+            agregarPaciente(head, nombre, edad, peso, altura);
+
+        } 
+        //muestra todos los pacientes
+        else if (opcion == 2)
+        {
+           imprimirPaciente(head);
+        }
+        //elimina pidiendo el nombre del sujeto a eliminar
+        else if (opcion == 3)
+        {
+            string nombre;
+            cout << "Indique el nombre\n";
+            cin >> nombre;
+
+            eliminarPaciente(head, nombre);
+        }
+        
+
+    }
+    //liberar memoria
+    while (head != nullptr) {
+        Paciente* temp = head;
+        head = head->next;
+        delete temp;
+    }
     
 }
-
-//     //creacion de arreglo
-//     Paciente pacientes[3];
-
-//     pacientes[0].nombre = "Maria";
-//     pacientes[0].edad = 45;
-//     pacientes[0].peso = 54.5;
-//     pacientes[0].altura = 1.65;
-
-//     pacientes[1].nombre = "Juana";
-//     pacientes[1].edad = 65;
-//     pacientes[1].peso = 59.0;
-//     pacientes[1].altura = 1.55;
-
-//     pacientes[2].nombre = "Matias";
-//     pacientes[2].edad = 29;
-//     pacientes[2].peso = 88.0;
-//     pacientes[2].altura = 1.80;
-
